e4f52b57d5ea2ee4be1da81cbd7d4f2b6038d1c3
Taher Hakim <85365433+tmhakim@users.noreply.github.com>
2026-03-03 12:24:42 +0400

    Import firmware and blobs from V1UOS35H.74-16-4-9

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),malmo)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,954cef441c2fa34a9bd41c28b2d40bfb2308939a)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,963cc92409bd880b708169bcbaf79f9f1df79887)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,5e301775f6f69e292dc0fb563c8d3a4e45c62aba)
+$(call add-radio-file-sha1-checked,radio/dsp.img,291e6a532b234c122f4d972b2da1e2846d6fe487)
+$(call add-radio-file-sha1-checked,radio/fsg.img,da040d6131afaee7edfe3a0eb21aa39ab4bd1f88)
+$(call add-radio-file-sha1-checked,radio/hyp.img,0ac6412d73cdd9b1fb2a1aa417ad1f6db92ef747)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,d00c1a8004a75640cca6f584e1dd8cb8173c964a)
+$(call add-radio-file-sha1-checked,radio/logo.img,d1697f0c0ecadce15ac2cfe38e213d691358f240)
+$(call add-radio-file-sha1-checked,radio/modem.img,56aebacc15de242e677f23dd8313414585019116)
+$(call add-radio-file-sha1-checked,radio/prov.img,b78019ac79ddf3d55149d37416d6b605bb911da6)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,bdf72cc55ef0dcacdf5474b494b1eb10e93e366e)
+$(call add-radio-file-sha1-checked,radio/rpm.img,2afd5a0ad76cabd5876b8935d1ce220305b38255)
+$(call add-radio-file-sha1-checked,radio/storsec.img,d0e639bb95b65b6342d77c088e9316b9b86efc47)
+$(call add-radio-file-sha1-checked,radio/tz.img,9cb2bc318a4b30289c7eed872b3066a4df2103e4)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,515eb445db4bfd1024d5b51b5891d761ea9e8518)
+$(call add-radio-file-sha1-checked,radio/xbl.img,032fbf0f4a0f3ddbefae2f795c9b85f6c22d5b87)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,8df70c8fbdad70a86e14121b94ba61c9165e803d)
+
+endif
